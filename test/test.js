7e105bc46848c25fec2be8e0651543fb6195dc8d
ntuvera <ntuvera@gmail.com>
2016-11-10 10:34:13 -0500

    template tests written for basic index route and json add calculation

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var supertest = require('supertest');
 var should = require('should');
 
-var server = supertest.agent('');
+var server = supertest.agent('http://192.81.211.198:3000');
 
 describe('sample unit test', function(){
   
@@ -17,7 +17,7 @@ describe('sample unit test', function(){
     .expect(200)
     .end(function(err,res){
       // HTTP status should be 200
-      res.status.shoudl.equal(200);
+      res.status.should.equal(200);
       // Error key should be false.
       res.body.error.should.equal(false);
       done();
@@ -36,6 +36,30 @@ describe('sample unit test', function(){
       res.status.should.equal(200);
       res.body.error.should.equal(false);
       res.body.data.should.equal(30);
+      done();
+    });
+  });
+
+  it("should not do thing incorrectly", function(done){
+    server
+    .post('/add')
+    .send({num1:10, num2:20})
+    .expect("Content-type", /json/)
+    .expect(200)
+    .end(function(err,res){
+      res.status.should.equal(200);
+      res.body.error.should.equal(false);
+      res.body.data.should.not.equal(40);
+      done();
+    });
+  });
+
+  it("should return a 404 if no page found", function(done){
+    server
+    .get('/falseRoute')
+    .expect(404)
+    .end(function(err,res){
+      res.status.should.equal(404) 
       done();
     });
   });
